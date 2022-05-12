3ef11b3a9678ed4f3e087f1b06cd1c1316dfdb8b
emanuelpasca <pasca.emanuel@yahoo.com>
2022-05-12 18:33:46 +0300

    Updates

--- backend/backend.py
+++ backend/backend.py
@@ -6,8 +6,86 @@ import pytesseract
 from flask_cors import CORS
 from werkzeug.utils import secure_filename
 import cv2
+import numpy as np
+
+# ------------------------------------------------------------------------------------------
 
 pytesseract.pytesseract.tesseract_cmd = 'C:\\Program Files\\Tesseract-OCR\\tesseract.exe'
+
+# ------------------------------------------------------------------------------------------
+
+def noise_removal(image):
+    import numpy as np
+    kernel = np.ones((1, 1), np.uint8)
+    image = cv2.dilate(image, kernel, iterations=1)
+    kernel = np.ones((1, 1), np.uint8)
+    image = cv2.erode(image, kernel, iterations=1)
+    image = cv2.morphologyEx(image, cv2.MORPH_CLOSE, kernel)
+    image = cv2.medianBlur(image, 3)
+    return (image)
+
+def thin_font(image):
+    image = cv2.bitwise_not(image)
+    kernel = np.ones((2,2),np.uint8)
+    image = cv2.erode(image, kernel, iterations=1)
+    image = cv2.bitwise_not(image)
+    return (image)
+
+def thick_font(image):
+    import numpy as np
+    image = cv2.bitwise_not(image)
+    kernel = np.ones((2,2),np.uint8)
+    image = cv2.dilate(image, kernel, iterations=1)
+    image = cv2.bitwise_not(image)
+    return (image)
+
+def getSkewAngle(cvImage) -> float:
+    # Prep image, copy, convert to gray scale, blur, and threshold
+    newImage = cvImage.copy()
+    gray = cv2.cvtColor(newImage, cv2.COLOR_BGR2GRAY)
+    blur = cv2.GaussianBlur(gray, (9, 9), 0)
+    thresh = cv2.threshold(blur, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1]
+
+    # Apply dilate to merge text into meaningful lines/paragraphs.
+    # Use larger kernel on X axis to merge characters into single line, cancelling out any spaces.
+    # But use smaller kernel on Y axis to separate between different blocks of text
+    kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (30, 5))
+    dilate = cv2.dilate(thresh, kernel, iterations=2)
+
+    # Find all contours
+    contours, hierarchy = cv2.findContours(dilate, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+    contours = sorted(contours, key = cv2.contourArea, reverse = True)
+    for c in contours:
+        rect = cv2.boundingRect(c)
+        x,y,w,h = rect
+        cv2.rectangle(newImage,(x,y),(x+w,y+h),(0,255,0),2)
+
+    # Find largest contour and surround in min area box
+    largestContour = contours[0]
+    print (len(contours))
+    minAreaRect = cv2.minAreaRect(largestContour)
+    cv2.imwrite("temp/boxes.jpg", newImage)
+    # Determine the angle. Convert it to the value that was originally used to obtain skewed image
+    angle = minAreaRect[-1]
+    if angle < -45:
+        angle = 90 + angle
+    return -1.0 * angle
+# Rotate the image around its center
+def rotateImage(cvImage, angle: float):
+    newImage = cvImage.copy()
+    (h, w) = newImage.shape[:2]
+    center = (w // 2, h // 2)
+    M = cv2.getRotationMatrix2D(center, angle, 1.0)
+    newImage = cv2.warpAffine(newImage, M, (w, h), flags=cv2.INTER_CUBIC, borderMode=cv2.BORDER_REPLICATE)
+    return newImage
+
+# Deskew image
+def deskew(cvImage):
+    angle = getSkewAngle(cvImage)
+    return rotateImage(cvImage, -1.0 * angle)
+
+
+# ------------------------------------------------------------------------------------------
 
 app = Flask(__name__)
 CORS(app)
@@ -21,10 +99,32 @@ def upload_file():
     path = os.path.join(user_folder, filename)
     file.save(path)
 
+    grayImg = cv2.imread(path, 0)
+
+    # Noise removal
+    no_noise = noise_removal(grayImg)
+    cv2.imwrite(path, no_noise)
+
+    # Dilation and Erosion
+    eroded_image = thin_font(no_noise)
+    cv2.imwrite(path, eroded_image)
+
     img = Image.open(path)
 
     result = pytesseract.image_to_string(img)
 
-    #os.remove(path)
+    # os.remove(path)
+
+    if result == "":
+        # Deskew image
+        new = cv2.imread(path)
+        fixed = deskew(new)
+        cv2.imwrite(path, fixed)
+
+        img2 = Image.open(path)
+        result = pytesseract.image_to_string(img2)
+        return jsonify({'result': result})
 
     return jsonify({'result': result})
+
+# ------------------------------------------------------------------------------------------
